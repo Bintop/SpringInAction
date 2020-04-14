3485c9955e25c35fe3f84718f78d2c2cb43b5646
bintop <923244771@qq.com>
2020-04-14 23:35:45 +0800

    第三章源码。

---
+++ ch03-jdbc/src/test/java/tacos/TacoCloudApplicationCh03JdbcTests.java
@@ -0,0 +1,16 @@
+package tacos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TacoCloudApplicationCh03JdbcTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
